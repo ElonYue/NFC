aed38a66a1e0810aade73dbbb1bf83923e9f50a6
chengyue80 <chengyue2020@163.com>
2020-06-22 19:06:33 +0800

    优化

--- app/src/main/java/com/cheng/nfc/NfcTechActivity.java
+++ app/src/main/java/com/cheng/nfc/NfcTechActivity.java
@@ -10,11 +10,13 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.cheng.nfc.utils.ByteHex;
+import com.cheng.nfc.utils.Constant;
 import com.cheng.nfc.utils.LogUtil;
 import com.cheng.nfc.utils.NfcUtils;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,15 +24,11 @@ public class NfcTechActivity extends AppCompatActivity {
     private static boolean READ_LOCK = false;
 
     private static final String TAG = "NfcTechActivity";
-
     IsoDep nfcA;
-
     NfcUtils nfcUtils;
-
-    TextView tv_cardId;
-
     TextView tv_type;
-
+    TextView tv_cardId;
+    TextView tv_result;
 
     @Override
     protected void onCreate(Bundle paramBundle) {
@@ -40,9 +38,20 @@ public class NfcTechActivity extends AppCompatActivity {
 
         this.tv_type = (TextView) findViewById(R.id.tv_type);
         this.tv_cardId = (TextView) findViewById(R.id.tv_cardId);
+        this.tv_result = (TextView) findViewById(R.id.tv_result);
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View param1View) {
+                String[] commands = new String[]{"80F6000304", "00B081001E", "00B0820002",
+                        "00A4000002DF01", "00B0810027", "00B0820065"};
+                int result = transceive(commands);
+                if (result == 0) {
+                    LogUtil.d("响应：" + Arrays.toString(commands));
+                    tv_result.setText(Arrays.toString(commands));
+                } else {
+                    LogUtil.d(String.format("操作失败，错误码=【%d】", result));
+                    tv_result.setText(String.format("操作失败，错误码=【%d】", result));
+                }
             }
         });
     }
@@ -79,45 +88,68 @@ public class NfcTechActivity extends AppCompatActivity {
         READ_LOCK = true;
         LogUtil.i("action = " + intent.getAction());
         if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
-            try {
-
-                IsoDep isoDep = IsoDep.get(tag);
-                this.nfcA = isoDep;
-                isoDep.connect();
-                transceive(new String[]{"80F6000304", "00B081001E", "00B0820002", "00A4000002DF01", "00B0810027", "00B0820065"});
-            } catch (IOException e) {
-                e.printStackTrace();
-                LogUtil.e(TAG, "读卡失败: " + e.getMessage());
-            } catch (Exception e) {
-                e.printStackTrace();
-                LogUtil.e(TAG, "读卡失败: " + e.getMessage());
-            } finally {
-            }
+            connect(tag);
         }
         READ_LOCK = false;
+    }
+
+    private int connect(Tag tag) {
+        if (this.nfcA != null && this.nfcA.isConnected()) {
+            return 0;
+        }
+        this.nfcA = IsoDep.get(tag);
+        try {
+            this.nfcA.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            LogUtil.e("连接失败：" + e.getMessage());
+            return Constant.DEVICE_CONNECT_FAIL;
+        }
+        return 0;
     }
 
     public void getNdefMsg(Tag tag) {
         String[] techList = tag.getTechList();
         StringBuilder sb = new StringBuilder();
         this.tv_type.setText(Arrays.toString(techList));
-
         String cardID = ByteHex.bytesToHex(tag.getId());
         Log.i(TAG, "cardID : " + cardID);
         this.tv_cardId.setText(cardID);
     }
 
 
-    private String[] transceive(String[] cmds) throws IOException {
-        String[] rspData = new String[cmds.length];
-        for (int i = 0; i < cmds.length; i++) {
-            String command = cmds[i];
-            LogUtil.i(TAG, "指令:" + command);
-            byte[] bytes = ByteHex.hexString2Bytes(command);
-            String result = ByteHex.bytesToHex(this.nfcA.transceive(bytes));
-            LogUtil.i(TAG, "响应:" + result);
-            rspData[i] = result;
+    private int transceive(String[] cosCmds) {
+        if (this.nfcA == null) {
+            return Constant.DEVICE_NOT_INIT;
+        } else if (!this.nfcA.isConnected()) {
+            return Constant.DEVICE_NOT_CONNECT;
         }
-        return rspData;
+        int cosNum = cosCmds.length;
+        String[] rspData = new String[cosNum];
+        try {
+            for (int i = 0; i < cosNum; i++) {
+                String command = cosCmds[i];
+                byte[] bytes = ByteHex.hexString2Bytes(command);
+                byte[] rsp = nfcA.transceive(bytes);
+                String result = ByteHex.bytesToHex(rsp);
+                rspData[i] = result.toUpperCase();
+            }
+        } catch (Exception e) {
+            LogUtil.e(e.getMessage());
+            return Constant.COMMAND_EXCEPTION;
+        }
+
+        for (int j = 0; j < cosNum; j++) {
+            String cosCmd = String.format(Locale.CHINA, "指令[%d]: %s", j, cosCmds[j]);
+            LogUtil.i(TAG, cosCmd.toUpperCase());
+        }
+
+        for (int j = 0; j < cosNum; j++) {
+            String rsp = String.format(Locale.CHINA, "响应[%d]: %s", j, rspData[j]);
+            LogUtil.i(TAG, rsp.toUpperCase());
+        }
+
+        System.arraycopy(rspData, 0, cosCmds, 0, cosNum);
+        return 0;
     }
 }
---
+++ app/src/main/java/com/cheng/nfc/utils/Constant.java
@@ -0,0 +1,28 @@
+package com.cheng.nfc.utils;
+
+/**
+ * @author : chengyue
+ * @date : 2020/6/23 09:20
+ * @history : change on 2020/6/23 09:20 by chengyue
+ * @since : v
+ */
+public class Constant {
+
+    /**
+     * NFC连接失败
+     */
+    public static final int DEVICE_CONNECT_FAIL = 10001;
+    /**
+     * NFC未初始化
+     */
+    public static final int DEVICE_NOT_INIT = 10002;
+    /**
+     * NFC未初始化
+     */
+    public static final int DEVICE_NOT_CONNECT = 10003;
+
+    /**
+     * 指令执行异常
+     */
+    public static final int COMMAND_EXCEPTION = 20001;
+}
